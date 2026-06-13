020531dabc1119b7a8a4b8d6144a89eb77fe6f74
Jesus Valencia <jesus.savs19@gmail.com>
2026-06-13 15:06:26 -0500

    feat: 🎸 add base config to upload image in cloudinary

--- config/plugins.ts
+++ config/plugins.ts
@@ -1,5 +1,25 @@
 import type { Core } from '@strapi/strapi';
 
-const config = ({ env }: Core.Config.Shared.ConfigParams): Core.Config.Plugin => ({});
+const config = ({ env }: Core.Config.Shared.ConfigParams): Core.Config.Plugin => ({
+    upload: {
+        config: {
+            provider: 'cloudinary',
+            providerOptions: {
+                cloud_name: env('CLOUDINARY_NAME'),
+                api_key: env('CLOUDINARY_KEY'),
+                api_secret: env('CLOUDINARY_SECRET'),
+            },
+            actionOptions: {
+                upload: {
+                    folder: 'jabes',
+                },
+                uploadStream: {
+                    folder: 'jabes',
+                },
+                delete: {},
+            },
+        },
+    },
+});
 
 export default config;
